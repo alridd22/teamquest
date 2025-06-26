97d2cd8cb9bb60ef8264ba91d0fc72e88eb1c1b4
alridd22 <alridd227@gmail.com>
2025-06-26 14:29:34 +0100

    Update register-team.js

--- netlify/functions/register-team.js
+++ netlify/functions/register-team.js
@@ -39,41 +39,18 @@ exports.handler = async (event, context) => {
 
     const sheetId = process.env.GOOGLE_SHEET_ID;
     const serviceAccountEmail = process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL;
+    
+    // Reconstruct private key from parts
+    const keyPart1 = process.env.GOOGLE_PRIVATE_KEY_PART1;
+    const keyPart2 = process.env.GOOGLE_PRIVATE_KEY_PART2;
+    const keyPart3 = process.env.GOOGLE_PRIVATE_KEY_PART3;
 
-    if (!sheetId || !serviceAccountEmail) {
+    if (!sheetId || !serviceAccountEmail || !keyPart1 || !keyPart2 || !keyPart3) {
       throw new Error('Missing environment variables');
     }
 
-    console.log('Setting up authentication');
-    
-    const privateKey = `-----BEGIN PRIVATE KEY-----
-MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCGw+YMg87AxUpz
-smaLvaq2ojIWpp0RRo1mTmk/grfaa4Mb4FOvAQdQy/4dWwat8lWe/HIG8kOLJzUD
-tfgIWi5MpQbTjDgq/T+tTyk4b1LA8jyQme3r5RHz4Yoj4ILmq3bC0EmifkIvs+iD
-Y1FhsmcMUFu7A+6ppCmD2vzGCvQA5nLNxlvgjTHtpWEME4h45kXEynM6+tc4PdEv
-6ko7FSbcjwaKj7yygT8hSFFZjhxuPobcZP7ld1GxmaGIwSvAOiDbXTR388tqZHLW
-MnvYE6AQ1tEbPBZh0EOZ0E9pTDYYjsryYZkQbTSR/L78aTA31ubgI4RkCZ1tRW4V
-Qe8OZm+fAgMBAAECggEAPrTfkdow9Yy3kG8l/QbTbOi6ssRzGEtCVyTMH0O1Rbo6
-BtoSRj+NxmAtDT7CNGUqxvADJM1MdW7YYbIlx8kiewJc00mPBx3Qp9VKQlq2YFY2
-rTgat2VevaKn8vqwHiIRgCOH58alCGpHmf84KmPnRBkOHc5+GkxrDtHyRTpqgBIN
-25yNXmseD0SmpktJyHojY3tjJvXQQIaWzj8OmuImKxDwLCFSp7FmHqcjj5avBGxb
-ax6FHlDBxPToCaSq+QvU0aYH6PrnoCItw61EsJBt8+ZcAv9BOeEk4QTIZDYERfCp
-QhfuSab6yNXqVFY9xikC2t2mIJAcLDbigwzjRJlyNQKBgQC9uBMokCp70pCzpSxB
-8D6oBZMoupleDShXYNctTjBiB6jWBWl4URlf9wNs8xPy1vq8kjN88chYrx7GdJ9U
-u/UTHmAssD7xuu6f85AX8VlTE/ehLBd1zlFh/WiJBa5RdANtbAJsStawjnUqBt+o
-z0SVJrH16FDVRGxszklz4qzY5QKBgQC12OmyS96FNdeVcGPm9qEceeenxDu4K2q0
-/7hrWOwHsynEVW/RL0zMsQs0VuCXtwuDARYgH2Wmgrpp/nRmLaM1W8A5qJzy8gvm
-uUn1ebW3ibpp8/FYQ9ujokJuRYJPx5CDk7wmaGKI3hyS/Qd04KAcrX03nEm9Y+sV
-+KnPMAiyMwKBgBcFDs4NMBp2IjiqAgS+MdwRURnQCmvvDMYNag6FPjmm1EWjwnhm
-3r1WK4Q8ul7s1t+qnbS7YaPhG8rYu7x8UfBP7zt85yZNmEawNvuwvTXnZlSYFQCm
-WBmbteNqXQxlF0VfpaueKQ6jOnQvAVRqUdgHKfC8j5JRCxbosODu0CpBAoGAY9fQ
-IDv11jAEySEnSPWa6fao0X01yN3VvcE18YKmPWgKg6jhNvqAcVa3ryoQMFypLmmX
-S1XRywW18mJqaHugQ7i3pzDD08Q/8pNDX0tPEZHGT0xH8812N7bsQH1bmhAZcZTY
-1kGz8C/7glz5DhwS7Qv4V2MxPZIxziN0oLRf4e8CgYBu1VOlFl7uXaxMixPUZghU
-KBWnf3uAzsOnnYBflkMy6XzjOf4hs7a4hN3zLQaQf1wl3HJoCcbmWi9epQTTnZPb
-3d+UrOGXWrMK0SYM4eYywRTK7xwOY6/1Hqk4/p1SiISnMomjJx5vbPu3idnvJ9Dt
-xGMKYlytcRo0Hb0f9UTUmg==
------END PRIVATE KEY-----`;
+    console.log('Reconstructing private key from parts');
+    const privateKey = `${keyPart1}\n${keyPart2}\n${keyPart3}`;
 
     console.log('Creating JWT client');
     const serviceAccountAuth = new JWT({
